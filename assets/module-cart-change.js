772ff7a716da6c856cc565c9e22f8de40d815867
Tom Blanchard <yo@tomblanchard.co.uk>
2021-11-26 20:40:25 +0000

    Theme download

--- assets/module-cart-change.js
+++ assets/module-cart-change.js
@@ -2,6 +2,41 @@ import EventEmitter from 'wolfy87-eventemitter';
 import get_element_options from './fn-get-element-options';
 import Events from './object-events';
 
+if ((typeof Shopify) === 'undefined') { window.Shopify = {}; }
+  /* Get from cart.js returns the cart in JSON */
+  if ((typeof Shopify.getCart) === 'undefined') {
+  Shopify.getCart = function(callback, cart) {
+    var promise = jQuery.Deferred();
+    if( !callback){
+      callback = promise.resolve;
+    }
+    if(!cart){    
+      var o = (new Date).getTime();
+      jQuery.ajax({
+        type: "GET",
+        url: "/cart.js?_=" + o,
+        dataType: "json",
+        async: false,
+        success: function (cart, textStatus, xhr) {
+          if ((typeof callback) === 'function') {
+            return callback(cart,textStatus, xhr);
+          }
+          else if(typeof Shopify.onCartUpdate ==='function') {
+            return Shopify.onCartUpdate(cart);
+          }
+        }
+      })
+    }else{
+      if ((typeof callback) === 'function') {
+      callback(cart);
+    }else if(typeof Shopify.onCartUpdate ==='function') {
+      Shopify.onCartUpdate(cart);
+        }
+    }
+    return promise;
+  }
+}
+
 function Self(element, events, options, module) {
   element.addEventListener('click', (event) => {
     event.preventDefault();
@@ -23,7 +58,9 @@ function Self(element, events, options, module) {
     })
       .then((res) => res.json())
       .then((res) => {
-        Events.trigger('cartchange:success', [res]);
+        Shopify.getCart(function(res){
+          Events.trigger('cartchange:success', [res]);
+        },res);
       })
       .catch((err) => {
         console.log({err});
